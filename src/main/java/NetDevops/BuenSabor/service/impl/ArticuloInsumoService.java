95973e51a6c6fc19ec8a1a9ee830bd8ce7b70d2c
Sallei <nicosallei@gmail.com>
2024-05-16 19:54:47 -0300

    se agrega a todos los servicios la incorporacion de las imagenes en base64

--- src/main/java/NetDevops/BuenSabor/service/impl/ArticuloInsumoService.java
+++ src/main/java/NetDevops/BuenSabor/service/impl/ArticuloInsumoService.java
@@ -3,12 +3,14 @@ package NetDevops.BuenSabor.service.impl;
 import NetDevops.BuenSabor.entities.ArticuloInsumo;
 import NetDevops.BuenSabor.entities.ImagenArticulo;
 import NetDevops.BuenSabor.repository.IAriticuloInsumoRepository;
+import NetDevops.BuenSabor.repository.IArticuloManufacturadoDetalleRepository;
 import NetDevops.BuenSabor.repository.ImagenArticuloRepository;
 import NetDevops.BuenSabor.service.IArticuloInsumoService;
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.nio.file.Files;
@@ -26,6 +28,10 @@ public class ArticuloInsumoService implements IArticuloInsumoService {
     private IAriticuloInsumoRepository articuloInsumoRepository;
     @Autowired
     private ImagenArticuloRepository imagenRepository;
+    @Autowired
+    private IArticuloManufacturadoDetalleRepository articuloManufacturadoRepository;
+    @Autowired
+    Funcionalidades funcionalidades;
 
     //region Eliminacion
     @Override
@@ -33,6 +39,21 @@ public class ArticuloInsumoService implements IArticuloInsumoService {
 
         try {
             ArticuloInsumo articuloInsumo = articuloInsumoRepository.findById(id).orElseThrow(() -> new Exception("No se encontro el articulo"));
+
+            // Verificar si el ArticuloInsumo está asignado a algún ArticuloManufacturado
+            if (articuloManufacturadoRepository.existsByArticuloInsumo_IdAndEliminadoFalse(id)) {
+                throw new Exception("No se puede eliminar el articulo porque está asignado a un ArticuloManufacturado");
+            }
+
+            // Marcar las imágenes correspondientes al ArticuloInsumo como eliminadas
+            Set<ImagenArticulo> imagenes = articuloInsumo.getImagenes();
+            if (imagenes != null) {
+                for (ImagenArticulo imagen : imagenes) {
+                    imagen.setEliminado(true);
+                    imagenRepository.save(imagen);
+                }
+            }
+
             articuloInsumo.setEliminado(true);
             articuloInsumoRepository.save(articuloInsumo);
             return true;
@@ -45,120 +66,149 @@ public class ArticuloInsumoService implements IArticuloInsumoService {
 
     //region Mostrar Lista
     @Override
-    public List<ArticuloInsumo> mostrarLista() throws Exception {
-        try {
-            return articuloInsumoRepository.findByEliminadoFalse();
-        } catch (Exception e) {
-            throw new Exception(e);
+public List<ArticuloInsumo> mostrarLista() throws Exception {
+    try {
+        List<ArticuloInsumo> articulos = articuloInsumoRepository.findByEliminadoFalse();
+        for (ArticuloInsumo articulo : articulos) {
+            if (articulo.getImagenes() != null) {
+                for (ImagenArticulo imagen : articulo.getImagenes()) {
+                    try {
+                        String imagenBase64 = funcionalidades.convertirImagenABase64(imagen.getUrl());
+                        imagen.setUrl(imagenBase64); // Actualizar el campo url en ImagenArticulo con la imagen en base64
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
         }
+        return articulos;
+    } catch (Exception e) {
+        throw new Exception(e);
     }
+}
     //endregion
 
     //region Cargar
-    @Override
-    public ArticuloInsumo cargar(ArticuloInsumo articuloInsumo) throws Exception {
-        try {
+   @Override
+public ArticuloInsumo cargar(ArticuloInsumo articuloInsumo) throws Exception {
+    try {
 
-            if(articuloInsumoRepository.existsByCodigoAndEliminadoFalse(articuloInsumo.getCodigo())){
-                throw new Exception("Ya existe un articulo con ese codigo");
-            }
-            if (articuloInsumoRepository.existsByDenominacionAndEliminadoFalse(articuloInsumo.getDenominacion())){
-                throw new Exception("Ya existe un articulo con esa denominacion");
-            }
-
-//            for (ImagenArticulo imagen : articuloInsumo.getImagenes()) {
-//                imagen.setArticulo(articuloInsumo);
-//            }
-//            articuloInsumoRepository.save(articuloInsumo);
-//            return articuloInsumoRepository.findById(articuloInsumo.getId()).get();
-
-            if (articuloInsumo.getImagenes() != null) {
-                for (ImagenArticulo imagen : articuloInsumo.getImagenes()) {
-                    // Decodificar la imagen base64 en un array de bytes
-                    byte[] bytes = Base64.decodeBase64(imagen.getUrl());
-
-                    // Crear un InputStream a partir del array de bytes
-                    InputStream inputStream = new ByteArrayInputStream(bytes);
-
-                    // Generar un nombre de archivo único para cada imagen
-                    String filename = UUID.randomUUID().toString() + ".jpg";
-
-                    // Usar Files.copy() para guardar la imagen en el directorio deseado
-                    Path path = this.root.resolve(filename);
-                    Files.copy(inputStream, path);
-
-                    // Actualizar el campo url en ImagenArticulo
-                    imagen.setUrl(path.toString());
-                    imagen.setArticulo(articuloInsumo);
-                }
-            }
-
-            articuloInsumoRepository.save(articuloInsumo);
-            return articuloInsumoRepository.findById(articuloInsumo.getId()).get();
-
-
-
-
-        } catch (Exception e) {
-            //e.printStackTrace();
-            throw new Exception(e);
+        if(articuloInsumoRepository.existsByCodigoAndEliminadoFalse(articuloInsumo.getCodigo())){
+            throw new Exception("Ya existe un articulo con ese codigo");
         }
+        if (articuloInsumoRepository.existsByDenominacionAndEliminadoFalse(articuloInsumo.getDenominacion())){
+            throw new Exception("Ya existe un articulo con esa denominacion");
+        }
+
+        if (articuloInsumo.getImagenes() != null) {
+            for (ImagenArticulo imagen : articuloInsumo.getImagenes()) {
+                // Generar un nombre de archivo único para cada imagen
+                String filename = UUID.randomUUID().toString() + ".jpg";
+
+                // Utilizar la función guardarImagen de Funcionalidades para guardar la imagen
+                String ruta = funcionalidades.guardarImagen(imagen.getUrl(), filename);
+
+                // Actualizar el campo url en ImagenArticulo
+                imagen.setUrl(ruta);
+                imagen.setArticulo(articuloInsumo);
+            }
+        }
+
+        articuloInsumoRepository.save(articuloInsumo);
+        return articuloInsumoRepository.findById(articuloInsumo.getId()).get();
+
+    } catch (Exception e) {
+        throw new Exception(e);
     }
+}
 
     //endregion
 
     //region Buscar Por Id
     @Override
-    public ArticuloInsumo buscarPorId(Long id) throws Exception {
-        try {
-            ArticuloInsumo articuloInsumo = articuloInsumoRepository.findByIdAndEliminadoFalse(id);
-            if (articuloInsumo == null) {
-                throw new Exception("No se encontro el articulo");
-            }
-            return articuloInsumo;
-        } catch (Exception e) {
-            throw new Exception(e);
+public ArticuloInsumo buscarPorId(Long id) throws Exception {
+    try {
+        ArticuloInsumo articuloInsumo = articuloInsumoRepository.findByIdAndEliminadoFalse(id);
+        if (articuloInsumo == null) {
+            throw new Exception("No se encontro el articulo");
         }
+
+        if (articuloInsumo.getImagenes() != null) {
+            for (ImagenArticulo imagen : articuloInsumo.getImagenes()) {
+                try {
+                    String imagenBase64 = funcionalidades.convertirImagenABase64(imagen.getUrl());
+                    imagen.setUrl(imagenBase64); // Actualizar el campo url en ImagenArticulo con la imagen en base64
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
+        return articuloInsumo;
+    } catch (Exception e) {
+        throw new Exception(e);
     }
+}
 
     //endregion
 
     //region Actualizar
-    @Override
-    public ArticuloInsumo actualizar(Long id, ArticuloInsumo articuloInsumo) throws Exception {
-        try {
-            if (!articuloInsumoRepository.existsById(id)){
-                throw new Exception("No se encontro el articulo");
-            }
-
-            if (articuloInsumoRepository.existsByCodigoAndEliminadoFalseAndIdNot(articuloInsumo.getCodigo(), id)){
-                throw new Exception("Ya existe un articulo con ese codigo");
-            }
-            if (articuloInsumoRepository.existsByDenominacionAndEliminadoFalseAndIdNot(articuloInsumo.getDenominacion(), id)){
-                throw new Exception("Ya existe un articulo con esa denominacion");
-            }
-
-            //region Logica para eliminar Imagenes
-            Set<ImagenArticulo> imagenesViejas = imagenRepository.findByArticulo_Id(id);
-            Set<ImagenArticulo> imagenesNuevas = articuloInsumo.getImagenes();
-
-            imagenesViejas.forEach(imagenVieja -> {
-                if (!imagenesNuevas.contains(imagenVieja)) {
-                    imagenVieja.setEliminado(true);
-                    imagenVieja.setArticulo(null);
-                    imagenRepository.save(imagenVieja);
-                }
-            });
-            //endregion
-
-
-
-            return articuloInsumoRepository.save(articuloInsumo);
-
-        } catch (Exception e) {
-            throw new Exception(e);
+   @Override
+public ArticuloInsumo actualizar(Long id, ArticuloInsumo articuloInsumo) throws Exception {
+    try {
+        if (!articuloInsumoRepository.existsById(id)){
+            throw new Exception("No se encontro el articulo");
         }
-    }
 
+        if (articuloInsumoRepository.existsByCodigoAndEliminadoFalseAndIdNot(articuloInsumo.getCodigo(), id)){
+            throw new Exception("Ya existe un articulo con ese codigo");
+        }
+        if (articuloInsumoRepository.existsByDenominacionAndEliminadoFalseAndIdNot(articuloInsumo.getDenominacion(), id)){
+            throw new Exception("Ya existe un articulo con esa denominacion");
+        }
+
+        //region Logica para eliminar Imagenes
+        Set<ImagenArticulo> imagenesViejas = imagenRepository.findByArticulo_Id(id);
+        Set<ImagenArticulo> imagenesNuevas = articuloInsumo.getImagenes();
+
+        imagenesViejas.forEach(imagenVieja -> {
+            if (!imagenesNuevas.contains(imagenVieja)) {
+                imagenVieja.setEliminado(true);
+                imagenVieja.setArticulo(null);
+                imagenRepository.save(imagenVieja);
+            }
+        });
+
+        //region Logica para eliminar Imagenes base64
+        if (articuloInsumo.getImagenes() != null) {
+            for (ImagenArticulo imagen : articuloInsumo.getImagenes()) {
+                // Utilizar la función guardarImagen de Funcionalidades para guardar la imagen
+                String filename = UUID.randomUUID().toString() + ".jpg";
+                try {
+                    String rutaImagen = funcionalidades.guardarImagen(imagen.getUrl(), filename);
+                    imagen.setUrl(rutaImagen); // Actualizar el campo url en ImagenArticulo
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+                // Verificar si la imagen ya existe en el conjunto de imágenes viejas
+                boolean exists = imagenesViejas.stream().anyMatch(oldImage -> oldImage.getUrl().equals(imagen.getUrl()));
+
+                // Si la imagen no existe, marcarla como eliminada
+                if (!exists) {
+                    imagen.setEliminado(true);
+                    imagen.setArticulo(null);
+                    imagenRepository.save(imagen);
+                }
+            }
+        }
+        //endregion
+
+        return articuloInsumoRepository.save(articuloInsumo);
+
+    } catch (Exception e) {
+        throw new Exception(e);
+    }
+}
     //endregion
 }
---
+++ src/main/java/NetDevops/BuenSabor/service/impl/Funcionalidades.java
@@ -0,0 +1,52 @@
+package NetDevops.BuenSabor.service.impl;
+
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Service
+public class Funcionalidades {
+
+    @Value("${app.upload.dir:${user.home}}")
+    public String uploadDir;
+
+    public String guardarImagen(String base64Image, String fileName) throws IOException {
+        // Decodificar el string base64 a un array de bytes
+        byte[] decodedBytes = Base64.decodeBase64(base64Image);
+
+        // Crear un archivo en la carpeta designada con el nombre de archivo proporcionado
+        File file = new File(uploadDir + File.separator + fileName);
+        FileOutputStream fop = new FileOutputStream(file);
+
+        // Escribir el array de bytes en el archivo
+        fop.write(decodedBytes);
+        fop.flush();
+        fop.close();
+
+        // Crear una ruta para el archivo
+        Path path = Paths.get(uploadDir, fileName);
+        String ruta = path.toString();
+
+        // Aquí puedes guardar la ruta en la base de datos y setearla en la clase correspondiente
+
+        return ruta;
+    }
+
+    public String convertirImagenABase64(String rutaImagen) throws IOException {
+        // Leer el archivo de la imagen en un array de bytes
+        byte[] bytesImagen = Files.readAllBytes(Paths.get(rutaImagen));
+
+        // Codificar el array de bytes a un string en formato base64
+        String imagenBase64 = Base64.encodeBase64String(bytesImagen);
+
+        return imagenBase64;
+    }
+
+}
