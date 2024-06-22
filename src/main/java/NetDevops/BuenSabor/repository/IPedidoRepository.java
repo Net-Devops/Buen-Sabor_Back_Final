d53a675699e370f801f64189b54c1b5e13fb4b26
Sallei <nicosallei@gmail.com>
2024-06-22 15:19:39 -0300

    actualizacion

--- src/main/java/NetDevops/BuenSabor/repository/IPedidoRepository.java
+++ src/main/java/NetDevops/BuenSabor/repository/IPedidoRepository.java
@@ -4,6 +4,9 @@ import NetDevops.BuenSabor.entities.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IPedidoRepository extends JpaRepository<Pedido, Long>{
+    List<Pedido> findBySucursal_Id(Long sucursalId);
 }
