024b29fb4cb30c93eafc1e070711fd4b92a20213
EMGomez32 <gomez.emiliano.martin@gmail.com>
2024-06-25 22:59:38 -0300

    empleado

--- src/main/java/NetDevops/BuenSabor/service/impl/LocalService.java
+++ src/main/java/NetDevops/BuenSabor/service/impl/LocalService.java
@@ -335,6 +335,7 @@ public ArticuloInsumo aumentarStock(Long id, Integer cantidad, Double nuevoPreci
     dto.setId(articulo.getId());
     dto.setCodigo(articulo.getCodigo());
     dto.setDenominacion(articulo.getDenominacion());
+
     // Asegúrate de tener un método para obtener la URL de la imagen principal
         if (!articulo.getImagenes().isEmpty()) {
             ImagenArticulo primeraImagen = articulo.getImagenes().iterator().next();
