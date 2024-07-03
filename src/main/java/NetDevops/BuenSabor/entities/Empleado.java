bb034563225bab62287a889ce0bbc0a794460898
sallei <75142980+nicosallei@users.noreply.github.com>
2024-07-02 23:16:54 -0300

    haciendo que no se pueda repetir el mail

--- src/main/java/NetDevops/BuenSabor/entities/Empleado.java
+++ src/main/java/NetDevops/BuenSabor/entities/Empleado.java
@@ -1,6 +1,7 @@
 package NetDevops.BuenSabor.entities;
 
 import NetDevops.BuenSabor.enums.Rol;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
@@ -15,6 +16,7 @@ public class Empleado extends Base {
     private String nombre;
     private String apellido;
     private String Telefono;
+    @Column(unique = true)
     private String email;
     private LocalDate fechaNacimiento;
     private Rol rol;
